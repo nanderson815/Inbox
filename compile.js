5811b154b13ef91b516d87309a182acd5bb01b10
Noah Anderson <noaanderson@deloitte.com>
2021-02-18 09:36:09 -0500

    finished compile ;

--- compile.js
+++ compile.js
@@ -28,5 +28,5 @@ var input = {
     },
   },
 };
-
-console.log(JSON.parse(solc.compile(JSON.stringify(input))));
+// console.log(JSON.parse(solc.compile(JSON.stringify(input))).contracts['Inbox.sol']);
+module.exports = JSON.parse(solc.compile(JSON.stringify(input))).contracts['Inbox.sol']
